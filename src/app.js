86a29e7eaaf771fbfb9c0054ef66fd534666a46e
Mono Wu <monowu.tw@gmail.com>
2016-08-02 22:53:16 +0800

    no message

--- src/app.js
+++ src/app.js
@@ -4,28 +4,34 @@ var HelloWorldLayer = cc.Layer.extend({
     ctor:function () {
         this._super();
 
-        var webView = new ccui.WebView("http://www.pinkoi.com/");
-
-        var scene = new cc.Scene();
-        var menu = new cc.Menu();
-        scene.addChild(menu);
         var win_size = cc.director.getWinSize();
-        menu.setPosition(win_size.width/2, win_size.height/2);
-        var item = new cc.MenuItemFont("Exit", function () {cc.director.popScene();}, this);
-        menu.addChild(item);
-        cc.director.pushScene(scene);
 
-        webView.setContentSize(1780, 960);
-        webView.setPosition(cc.p(cc.winSize.width/2, cc.winSize.height/2));
-        webView.setScalesPageToFit(true);
-        window.webView = webView;
-        this.addChild(window.webView);
+        var webView = new ccui.WebView();
 
         // var settingSpriteNormal = new cc.Sprite(res.SettingBTN);
         // var settingSpriteSelected = new cc.Sprite(res.SettingBTN_sel);
         // var settingMenuItem = new cc.MenuItemSprite(settingSpriteNormal, settingSpriteSelected, this.onSetting, this);
-        // settingMenuItem.setPosition(cc.p(cc.winSize.width/2, cc.winSize.height/2));
-        //
+        // settingMenuItem.setPosition(win_size.width/2, win_size.height/2);
+
+        var scene = new cc.Scene();
+        var menu = new cc.Menu();
+        scene.addChild(menu);
+
+        //https://github.com/cocos2d/cocos2d-js/issues/1795
+        menu.setPosition(win_size.width/2, win_size.height/2);
+        var item = new cc.MenuItemFont("Exit", function () {console.log("setting");}, this);
+        item.setPosition(win_size.width/2, win_size.height/2);
+        menu.addChild(item);
+        cc.director.pushScene(scene);
+
+        webView.setContentSize(1780, 960);
+        webView.setPosition(win_size.width/2, win_size.height/2);
+        //webView.loadURL("http://www.pinkoi.com/");
+        webView.setScalesPageToFit(true);
+        window.webView = webView;
+        this.addChild(window.webView);
+
+
         // var menu = new cc.Menu(settingMenuItem);
         // menu.setPosition(cc.p(0, 0));
         // this.addChild(menu);
