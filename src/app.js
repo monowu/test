eb8e6084e12803d5dd34c48a5a2bb007305ea5b9
Mono Wu <monowu.tw@gmail.com>
2016-08-03 22:44:50 +0800

    no message

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-
 var HelloWorldLayer = cc.Layer.extend({
     sprite:null,
     ctor:function () {
@@ -8,36 +7,19 @@ var HelloWorldLayer = cc.Layer.extend({
 
         var webView = new ccui.WebView();
 
-        // var settingSpriteNormal = new cc.Sprite(res.SettingBTN);
-        // var settingSpriteSelected = new cc.Sprite(res.SettingBTN_sel);
-        // var settingMenuItem = new cc.MenuItemSprite(settingSpriteNormal, settingSpriteSelected, this.onSetting, this);
-        // settingMenuItem.setPosition(win_size.width/2, win_size.height/2);
-
-        var scene = new cc.Scene();
-        var menu = new cc.Menu();
-        scene.addChild(menu);
-
-        //https://github.com/cocos2d/cocos2d-js/issues/1795
-        menu.setPosition(win_size.width/2, win_size.height/2);
-        var item = new cc.MenuItemFont("Exit", function () {console.log("setting");}, this);
-        item.setPosition(win_size.width/2, win_size.height/2);
-        menu.addChild(item);
-        cc.director.pushScene(scene);
-
-        webView.setContentSize(1780, 960);
+        webView.setContentSize(1024, 768);
         webView.setPosition(win_size.width/2, win_size.height/2);
-        //webView.loadURL("http://www.pinkoi.com/");
+        webView.loadURL("http://www.pinkoi.com/");
         webView.setScalesPageToFit(true);
+
         window.webView = webView;
         this.addChild(window.webView);
 
-
-        // var menu = new cc.Menu(settingMenuItem);
-        // menu.setPosition(cc.p(0, 0));
-        // this.addChild(menu);
+        var backBtn = document.getElementById('backBtn');
+        backBtn.style.visibility = 'visible';
     },
-    onSetting: function () {
-        console.log("setting");
+    removeWebView: function () {
+        console.log("removeWebView");
     }
 });
 
